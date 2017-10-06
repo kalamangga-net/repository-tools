4eb55ab6b663cd6f7060811bba586413a5900696
Yudha H Tejaningrat <yudha_ht@yahoo.com>
2017-10-06 16:35:38 +0700

    load-mysql: debian-jessie & blankon-tambora

---
+++ load-mysql/debian-jessie.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+REPO_LST="/tmp/repo/jessie.list"
+REPO_SQL="/tmp/repo/jessie.sql"
+cat $REPO_LIST | \
+   awk 'BEGIN { FS=";" } { print "INSERT INTO `jessie` VALUES (\"" $1 "\",\"" $2 "\",\"" $3 "\");"}' \
+   > "$REPO_SQL"
+mysql -u yht -p packages < "$REPO_SQL"
