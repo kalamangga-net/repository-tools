092ee8a227a2b818271da8028331316dd28452b8
Yudha H Tejaningrat <yudha_ht@yahoo.com>
2017-10-06 18:09:24 +0700

    load-mysql: fix minor

--- load-mysql/blankon-tambora.sh
+++ load-mysql/blankon-tambora.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 REPO_LST="/tmp/repo/tambora.list"
 REPO_SQL="/tmp/repo/tambora.sql"
-cat $REPO_LIST | \
+cat $REPO_LST | \
    awk 'BEGIN { FS=";" } { print "INSERT INTO `jessie` VALUES (\"" $1 "\",\"" $2 "\",\"" $3 "\");"}' \
    > "$REPO_SQL"
 mysql -u yht -p packages < "$REPO_SQL"
